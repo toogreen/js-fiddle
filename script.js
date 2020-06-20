78ace686cbfbf114e538a4edf6fea336c7c449ee
David Gagnon <toogreen@gmail.com>
2020-06-20 02:47:23 -0400

    added old scripts and css i created for McCarthy

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var date = new Date();
-document.body.innerHTML = "<h1>Today is " + date  + "</h1>";
+document.body.innerHTML = "<h1>Today is " + date  + "</h1><a href='mccarthy-grid.html'>mccarthy-grid.html</a><br><a href='mccarthy-new-grid.html'>mccarthy-new-grid.html</a>";
